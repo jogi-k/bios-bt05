ec131900559ebc374ea9dc12097b4971d14ad5ac
Jogi Kuenstner <jogi@kuenstner.de>
2015-03-29 23:05:17 +0200

    corrected indention

--- bios-bt05/bios-bt05.ino
+++ bios-bt05/bios-bt05.ino
@@ -1,5 +1,5 @@
 
-#define KEY_SHIFT  KEY_LEFT_SHIFT 
+#define KEY_SHIFT  KEY_LEFT_SHIFT
 
 /* do it only once */
 int done = 0;
@@ -34,107 +34,107 @@ void loop()
     {
         // START copy between teensy and sketchbook
 
-    // Enter BIOS via pressing 10 times DELETE
-    for ( i = 0; i < 10; ++i )
-    {
-	usb_keyboard_press( KEY_DELETE, 0 );
-	_delay_ms(1000);
-    }
+        // Enter BIOS via pressing 10 times DELETE
+            for ( i = 0; i < 10; ++i )
+        {
+            usb_keyboard_press( KEY_DELETE, 0 );
+            _delay_ms(1000);
+        }
 
-    // Go right into the menu
-    usb_keyboard_press( KEY_RIGHT , 0  );
-    _delay_ms(500);
-    // Go down to USB-Boot
-    for ( i = 0; i < 8; ++i )
-    {
-	usb_keyboard_press( KEY_DOWN, 0 );
-	_delay_ms(500);
-    }
-    // and enable USB-Boot
-    usb_keyboard_press( KEY_ENTER , 0 );
-    _delay_ms(500);
-    usb_keyboard_press( KEY_PAGE_DOWN , 0 );
-    _delay_ms(500);
-    usb_keyboard_press( KEY_ENTER , 0 );
-    _delay_ms(500);
-    usb_keyboard_press( KEY_PAGE_DOWN , 0 );
-    _delay_ms(500);
-    usb_keyboard_press( KEY_ENTER , 0 );
-    _delay_ms(500);
-
-    // return from submenu
-    usb_keyboard_press( KEY_ESC , 0 );
-    _delay_ms(500);
-
-    // go to boot-order-menu
-    for( i = 0; i < 3; ++i )
-    {
-        usb_keyboard_press( KEY_RIGHT , 0);
+        // Go right into the menu
+        usb_keyboard_press( KEY_RIGHT , 0  );
         _delay_ms(500);
-    }
+        // Go down to USB-Boot
+        for ( i = 0; i < 8; ++i )
+        {
+            usb_keyboard_press( KEY_DOWN, 0 );
+            _delay_ms(500);
+        }
+        // and enable USB-Boot
+        usb_keyboard_press( KEY_ENTER , 0 );
+        _delay_ms(500);
+        usb_keyboard_press( KEY_PAGE_DOWN , 0 );
+        _delay_ms(500);
+        usb_keyboard_press( KEY_ENTER , 0 );
+        _delay_ms(500);
+        usb_keyboard_press( KEY_PAGE_DOWN , 0 );
+        _delay_ms(500);
+        usb_keyboard_press( KEY_ENTER , 0 );
+        _delay_ms(500);
 
-    // go down to to first entry
-    for( i = 0; i < 3; ++i )
-    {
-        usb_keyboard_press( KEY_DOWN , 0);
-    _delay_ms(500);
-    }
-    // Enter to select this entry
-    usb_keyboard_press( KEY_ENTER , 0 );
-    _delay_ms(500);
+        // return from submenu
+        usb_keyboard_press( KEY_ESC , 0 );
+        _delay_ms(500);
 
-    // Page up is upper value = uefi menu
-    usb_keyboard_press( KEY_PAGE_UP , 0 );
-    _delay_ms(500);
+        // go to boot-order-menu
+        for( i = 0; i < 3; ++i )
+        {
+            usb_keyboard_press( KEY_RIGHT , 0);
+            _delay_ms(500);
+        }
 
-    // and enter
-    usb_keyboard_press( KEY_ENTER , 0 );
-    _delay_ms(500);
+        // go down to to first entry
+        for( i = 0; i < 3; ++i )
+        {
+            usb_keyboard_press( KEY_DOWN , 0);
+        _delay_ms(500);
+        }
+        // Enter to select this entry
+        usb_keyboard_press( KEY_ENTER , 0 );
+        _delay_ms(500);
 
-    // F4 = Save and exit
-    usb_keyboard_press( KEY_F4 , 0 );
-    _delay_ms(500);
+        // Page up is upper value = uefi menu
+        usb_keyboard_press( KEY_PAGE_UP , 0 );
+        _delay_ms(500);
 
-    // Yes, I really want to save and exit
-    usb_keyboard_press( KEY_ENTER , 0 );
-    _delay_ms(500);
+        // and enter
+        usb_keyboard_press( KEY_ENTER , 0 );
+        _delay_ms(500);
 
-    // Yes, I really want to save and exit
-    usb_keyboard_press( KEY_ENTER , 0 );
-    _delay_ms(500);
+        // F4 = Save and exit
+        usb_keyboard_press( KEY_F4 , 0 );
+        _delay_ms(500);
 
-    /* now we are in the UEFI-shell */
-    // go to fs0:
-    usb_keyboard_press( KEY_F, 0 );
-    _delay_ms(100);
-    usb_keyboard_press( KEY_S, 0 );
-    _delay_ms(100);
-    usb_keyboard_press( KEY_0, 0 );
-    _delay_ms(100);
-    usb_keyboard_press( KEY_SEMICOLON, KEY_SHIFT );
-    _delay_ms(100);
-    usb_keyboard_press( KEY_ENTER, 0 );
-    _delay_ms(1000 );
+        // Yes, I really want to save and exit
+        usb_keyboard_press( KEY_ENTER , 0 );
+        _delay_ms(500);
 
-    /* now we are on the USB-Stick */
-    // and now run "go_x64"
-    usb_keyboard_press( KEY_G, 0 );
-    _delay_ms(100);
-    usb_keyboard_press( KEY_O , 0);
-    _delay_ms(100);
-    usb_keyboard_press( KEY_MINUS, KEY_SHIFT );
-    _delay_ms(100);
-    usb_keyboard_press( KEY_X, 0 );
-    _delay_ms(100);
-    usb_keyboard_press( KEY_6, 0 );
-    _delay_ms(100);
-    usb_keyboard_press( KEY_4, 0 );
+        // Yes, I really want to save and exit
+        usb_keyboard_press( KEY_ENTER , 0 );
+        _delay_ms(500);
 
-    // last chance to escape via removing power from IPC
-    _delay_ms(4000);
+        /* now we are in the UEFI-shell */
+        // go to fs0:
+        usb_keyboard_press( KEY_F, 0 );
+        _delay_ms(100);
+        usb_keyboard_press( KEY_S, 0 );
+        _delay_ms(100);
+        usb_keyboard_press( KEY_0, 0 );
+        _delay_ms(100);
+        usb_keyboard_press( KEY_SEMICOLON, KEY_SHIFT );
+        _delay_ms(100);
+        usb_keyboard_press( KEY_ENTER, 0 );
+        _delay_ms(1000 );
 
-    usb_keyboard_press( KEY_ENTER, 0 );
-    _delay_ms(100);
+        /* now we are on the USB-Stick */
+        // and now run "go_x64"
+        usb_keyboard_press( KEY_G, 0 );
+        _delay_ms(100);
+        usb_keyboard_press( KEY_O , 0);
+        _delay_ms(100);
+        usb_keyboard_press( KEY_MINUS, KEY_SHIFT );
+        _delay_ms(100);
+        usb_keyboard_press( KEY_X, 0 );
+        _delay_ms(100);
+        usb_keyboard_press( KEY_6, 0 );
+        _delay_ms(100);
+        usb_keyboard_press( KEY_4, 0 );
+
+        // last chance to escape via removing power from IPC
+        _delay_ms(4000);
+
+        usb_keyboard_press( KEY_ENTER, 0 );
+        _delay_ms(100);
 
 
 
